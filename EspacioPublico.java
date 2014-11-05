1c2cb6c6adf5e8baa52ccbdb3a0f9d01c4923644
merrash <merrash@gmail.com>
2014-11-05 22:46:57 +0100

    Create EspacioPublico.java

---
+++ EspacioPublico.java
@@ -0,0 +1,18 @@
+public class EspacioPublico {
+    private String direccion;
+    private String tipoEspacio;
+
+    public void setDireccion(String dir) {
+		direccion = dir;
+	}
+    public String getDireccion() {
+		return direccion;
+	}
+
+    public void setTipoEspacio(String tipo) {
+		tipoEspacio = tipo;
+	}
+    public String getTipoEspacio() {
+		return tipoEspacio;
+	}
+}
