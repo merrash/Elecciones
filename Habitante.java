0d7223a098a9ee98d7f4fcfd87ad83e9b081bd63
merrash <merrash@gmail.com>
2014-11-05 22:45:22 +0100

    Create Habitante.java

---
+++ Habitante.java
@@ -0,0 +1,46 @@
+public class Habitante{
+	
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	
+
+	
+	public void setDni(String ni) {
+		dni = ni;
+	}
+	public String getDni() {
+		return dni;
+	}
+
+	
+	public void setNombre(String nom) {
+		nombre = nom;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	
+	public void setApellidos(String apell) {
+		apellidos = apell;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	
+	public void setEdad(int ed) {
+		edad = ed;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+}
+    
+
