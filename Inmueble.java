c86e3f4a780c19393505c019b4386912de47c90e
merrash <merrash@gmail.com>
2014-11-05 22:46:08 +0100

    Create Inmueble.java

---
+++ Inmueble.java
@@ -0,0 +1,44 @@
+public class Inmueble {
+        
+        private String calle;
+	private int numero;
+	private int piso;
+	private String puerta;
+	
+
+	
+	public void setCalle(String cal) {
+		calle = cal;
+	}
+	public String getCalle() {
+		return calle;
+	}
+
+	
+	public void setNumero(int num) {
+		numero = num;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	
+	public void setPiso(int pis) {
+		piso = pis;
+	}
+
+	public int getPiso() {
+		return piso;
+	}
+
+	
+	public void setPuerta(String pu) {
+		puerta = pu;
+	}
+
+	public String getPuerta() {
+		return puerta;
+	}
+    
+}
