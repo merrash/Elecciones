f9301c39d7e17e43e94e13f33c2eeb92d2475828
merrash <merrash@gmail.com>
2014-11-05 22:47:40 +0100

    Create Ayuntamiento.java

---
+++ Ayuntamiento.java
@@ -0,0 +1,36 @@
+public class Ayuntamiento {
+    private String localidad;
+    private String provincia;
+    private String nomAlcalde;
+    private String partidoAlcalde;
+
+    
+    public void setLocalidad(String loc) {
+        localidad = loc;
+    }
+    public String getLocalidad(){
+        return localidad;
+    }
+    
+    public void setProvincia(String prov) {
+        provincia = prov;
+    }
+    public String getProvincia(){
+        return provincia;
+    }
+    
+    public void setNomAlcalde(String nomal) {
+        nomAlcalde = nomal;
+    }
+    public String getNomAlcalde(){
+        return nomAlcalde;
+    }
+    
+     public void setPartidoAlcalde(String partal) {
+        partidoAlcalde = partal;
+    }
+    public String getPartidoAlcalde(){
+        return partidoAlcalde;
+    }
+    
+}
