b6316812dcb25e636d3c1f3d5b3f8af30f6e83fa
merrash <merrash@gmail.com>
2014-11-05 22:48:19 +0100

    Create Partido.java

---
+++ Partido.java
@@ -0,0 +1,34 @@
+public class Partido {
+    private String nombrePartido;
+    private String alineacion;
+    private String sede;
+    private String presidente; 
+    
+    public void setNomrePartido(String nompar){
+        nombrePartido = nompar;
+    }
+    public String getNomrePartido(){
+        return nombrePartido;
+    }
+    
+    public void setAlineacion(String alin){
+        alineacion = alin;
+    }
+    public String getAlineacion(){
+        return alineacion;
+    }
+    
+    public void setSede(String sed){
+        sede = sed;
+    }
+    public String getSede(){
+        return sede;
+    }
+    
+    public void setPresidente(String pres){
+        presidente = pres;
+    }
+    public String getPresidente(){
+        return presidente;
+    }
+}
